7dd8f949fea6c2a56ee8650891c3b55821440469
jsdegithub <def_myself@qq.com>
2020-09-04 14:22:25 +0800

    add note

--- js/Observer.js
+++ js/Observer.js
@@ -16,6 +16,7 @@ class Observer {
             enumerable: true,
             configurable: true,
             get() {
+                //若当前全局watcher有值，就将当前的全局watcher添加到dep实例的subs中去
                 Dep.target && dep.addSub(Dep.target);
                 return value;
             },
@@ -38,7 +39,6 @@ class Dep {
         this.subs.push(watcher);
     }
     notify() {
-        console.log('通知了观察者');
         this.subs.forEach(w => w.update());
     }
 }
@@ -50,6 +50,8 @@ class Watcher {
         this.callback = callback;
         this.oldVal = this.getOldVal();
     }
+    // getOldVal只有在 new Watcher的时候才会调用，而 new Watcher只有在 new Compiler时才会调用，
+    // 所以 getOldVal只有在初始页面渲染时才会调用一次，所以每个数据只会被添加一个 watcher。
     getOldVal() {
         Dep.target = this;
         let oldVal = compileUtil.getval(this.attr, this.vm);
